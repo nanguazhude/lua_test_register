f8c5f7374da885d4d7cf6ff94c9cd556ba8b09d7
nanguazhude <nanguazhude@vip.qq.com>
2018-09-29 19:17:40 +0800

    +test code

--- main.cpp
+++ main.cpp
@@ -29,8 +29,16 @@ public:
 #include <lua.hpp>
 #include <iostream>
 int map_function(lua_State *L) try{
- 
+
+    std::cout << "the stack size : " <<lua_gettop(L) <<std::endl ;
+    lua_pushvalue(L, lua_upvalueindex(1) );
+    std::cout << "upvalue is table : " <<lua_istable(L,-1) <<std::endl ;
+    lua_rawgeti(L,-1,1);
+    std::cout << lua_tointeger(L,-1) <<std::endl ;
+    lua_pop(L,2);
+
     lua_checkstack(L, 8);
+       
     const auto varType = lua_type(L,-1);
 
     if (varType == LUA_TNUMBER) {
@@ -52,6 +60,7 @@ int map_function(lua_State *L) try{
 
     lua_pushnil(L);
     return 1;
+
 }
 catch (const LuaCplusplusExceptioin & e) { 
     throw e;
@@ -101,6 +110,15 @@ int main(int argc, char ** argv) {
     auto L = luaL_newstate();
     luaL_openlibs(L);
     
+    {
+        lua_newtable(L);
+        lua_pushcclosure(L,&destory_function,1);
+        //lua_newtable(L);
+        //lua_setupvalue(L,-2,1);
+        lua_getupvalue(L,-1,1);
+        std::cout<<  lua_istable(L,-1) <<std::endl ;
+    }
+
     lua_checkstack(L,8);
     const auto varTop = lua_gettop(L);
     int varMetateTable;
@@ -109,26 +127,31 @@ int main(int argc, char ** argv) {
    
     auto varData = ::new(varMemory)  std::shared_ptr<void>();
     
-
     if (luaL_newmetatable(L, "std::shared_ptr<void>")) {
         varMetateTable = lua_gettop(L);
 
         lua_pushlstring(L,"__index",7);
-        lua_pushcfunction(L,&map_function);
+
+        lua_createtable(L, 1, 0);
+        lua_pushinteger(L,999);
+        lua_rawseti(L,-2,1);
+        lua_pushcclosure(L,&map_function,1);
+        //lua_pushcfunction(L, &map_function);
+        //lua_createtable(L, 0, 0);
+        //lua_setupvalue(L, -2, 0);
+
         lua_rawset(L, varMetateTable);
 
         lua_pushlstring(L, "__gc", 4);
         lua_pushcfunction(L, &destory_function);
         lua_rawset(L, varMetateTable);
         
-        
     }
         
     lua_setmetatable(L,-2);
 
     lua_setglobal(L,"bbc");
-
-
+    
     lua_settop(L,varTop);
 
     lua_pushcfunction(L,&show_error);
